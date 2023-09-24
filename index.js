75c687473647abbddb76aef7fd0a124d478bb22a
anvita-kumar30 <109106936+anvita-kumar30@users.noreply.github.com>
2023-09-24 15:41:12 +0000

    init

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ console.log(typeof ('Hello World'));
 var anina = "Anvita Kumar";
 
 console.log(anina);
+
+function aninaa() {
+  console.log("Kumar");
+  console.log("Anvita");
+  console.log("anina");
+}
+
+aninaa();
+
+function ani(name, age) {
+  console.log("Hi " + name + ", Your age is " + age);
+  return "all ok";
+}
+
+var flag = ani("Nia", "20");
+
+console.log(flag);
